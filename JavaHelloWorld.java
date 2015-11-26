eda7ea5760e7200cbacc050e1e2c3326a33e96a3
dakim <kkandori@gmail.com>
2015-11-25 16:00:01 -0800

    adding

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,7 @@ public class JavaHelloWorld
 	public static void main ( String [] args )
 	{
 		System.out.println("Java Hello World");
+		System.out.println("YAY!!!!!");
 		System.out.println("Hello again");
 	}
 }
